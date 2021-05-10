c8e317f8514557563662ad64ca0d91cb2274cb62
lmt6498 <lmt6498@users.noreply.github.com>
2021-05-10 11:33:10 +0700

    nop bai tap

---
+++ xuly.js
@@ -0,0 +1,9 @@
+var inputWidth = prompt("Enter the width");
+var inputHeight = prompt("Enter the height");
+//chuyển sang số nguyên
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+
+var area = width * height;
+
+document.write("Diện tích của hình chữ nhật là: " +area);
